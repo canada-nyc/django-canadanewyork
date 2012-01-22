11eda0f13cb1363b7d744f5e8ac52410ac3e21e2
Saul Shanabrook <saul.shanabrook@gmail.com>
2012-01-22 16:00:40 -0500

    heroku celery support

--- canada/settings.py
+++ canada/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS += (
     'djcelery',
     'djcelery_email',
     'celery',
+    'djkombu',
    )
 
 EMAIL_HOST = "smtp.gmail.com"
@@ -126,6 +127,50 @@ global_settings.TEMPLATE_CONTEXT_PROCESSORS += (
 INSTALLED_APPS += (
     'gunicorn',
     )
+
+import os
+import sys
+import urlparse
+
+# Register database schemes in URLs.
+urlparse.uses_netloc.append('postgres')
+urlparse.uses_netloc.append('mysql')
+
+try:
+
+    # Check to make sure DATABASES is set in settings.py file.
+    # If not default to {}
+
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except Exception:
+    print 'Unexpected error:', sys.exc_info()
+
+
+import djcelery
+djcelery.setup_loader()
+BROKER_BACKEND = "djkombu.transport.DatabaseTransport"
+CELERY_RESULT_DBURI = DATABASES['default']
 
 ########
 #Debug

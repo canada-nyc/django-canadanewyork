5266e0f1c96ec240955359f396c8024676a9eae7
Saul Shanabrook <s.shanabrook@gmail.com>
2012-07-18 10:56:18 -0400

    Fixed Bulkmail views and template tags

--- canada/apps/bulkmail/templatetags/bulkmail_tags.py
+++ canada/apps/bulkmail/templatetags/bulkmail_tags.py
@@ -1,15 +1,9 @@
 from django import template
 from django.contrib.sites.models import Site
 
-
-def do_domain(parser, token):
-    return DomainNode()
-
-
-class DomainNode(template.Node):
-    def render(self, context):
-        context['domain'] = Site.objects.get_current().domain[:-1]
-        return ''
-
 register = template.Library()
-register.tag('get_domain', do_domain)
+
+
+@register.filter
+def full_domain(value):
+    return '//' + Site.objects.get_current().domain + value
--- canada/functions.py
+++
@@ -1,8 +0,0 @@
-def add_to_middleware(MIDDLEWARE_CLASSES, middleware, prepend=False):
-    if middleware not in MIDDLEWARE_CLASSES:
-        if prepend:
-            return (middleware,) + MIDDLEWARE_CLASSES
-        else:
-            return MIDDLEWARE_CLASSES + (middleware,)
-    else:
-        return MIDDLEWARE_CLASSES

50210be4f6e0f8f2455295a4b04114201407fec0
Saul Shanabrook <s.shanabrook@gmail.com>
2012-09-02 14:41:39 -0400

    Restored Procfile and removed PREPEND_WWW from all settings

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn manage:application -b 0.0.0.0:$PORT
+worker: python worker.py
--- canada/settings/prod.py
+++ canada/settings/prod.py
@@ -6,7 +6,7 @@ import dj_database_url
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-PREPEND_WWW = True
+#PREPEND_WWW = True
 
 ########
 #Cache

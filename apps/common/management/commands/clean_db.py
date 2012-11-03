eaa792b22d7f06937525448db0c90d6f11215abc
Saul Shanabrook <s.shanabrook@gmail.com>
2012-11-03 12:43:40 -0400

    Fixed Superuser creation

--- apps/common/management/commands/clean_db.py
+++ apps/common/management/commands/clean_db.py
@@ -3,6 +3,7 @@ from subprocess import call
 
 from django.core.management.base import NoArgsCommand
 from django.core.management import call_command
+from django.contrib.auth.models import User
 
 
 class Command(NoArgsCommand):
@@ -27,11 +28,23 @@ class Command(NoArgsCommand):
         self.log('Migrating DB')
         call_command('migrate', fake=True, interactive=False, verbosity=0)
         self.log('Collecting Static DB')
-        call_command('collectstatic', interactive=False, verbosity=1)
-        self.log('Adding SU')
-        call_command('add_superuser')
+        call_command('collectstatic', interactive=False, verbosity=0)
+        self.create_superuser('saul', 'saul123', 's.shanabrook@gmail.com')
         self.log('Adding test_data')
         call_command('test_data')
+
+    def create_superuser(self, username, password, email):
+        self.log('Adding super user')
+        call_command('createsuperuser', interactive=False, username=username,
+                     email=email)
+
+        self.log('    Setting password')
+        user = User.objects.get(username=username)
+        user.set_password(password)
+        user.save()
+
+        self.log(('    username: {}\n'
+                  '    password: {}\n'.format(username, password)))
 
     def log(self, string):
         self.stdout.write(string + '\n')

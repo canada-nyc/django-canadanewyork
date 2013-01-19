9a384d45f7cc51d14dc0a0b2209b97f675e6cf6d
Saul Shanabrook <s.shanabrook@gmail.com>
2013-01-19 16:31:17 -0500

    Added sentry support fixed #32

--- configs/settings/prod.py
+++ configs/settings/prod.py
@@ -37,6 +37,65 @@ MIDDLEWARE_CLASSES = ('django.middleware.cache.UpdateCacheMiddleware',) + MIDDLE
 
 
 ###########
+# LOGGING #
+###########
+RAVEN_CONFIG = {
+    'dsn': os.environ.get('SENTRY_DSN'),
+}
+
+RQ_SENTRY_DSN = RAVEN_CONFIG['dns']
+
+INSTALLED_APPS = (
+    'raven.contrib.django.raven_compat',
+)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'root': {
+        'level': 'WARNING',
+        'handlers': ['sentry'],
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'sentry': {
+            'level': 'ERROR',
+            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'ERROR',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'raven': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+            'propagate': False,
+        },
+    },
+}
+
+MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES + (
+    'raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware',
+)
+
+###########
 # STORAGE #
 ###########
 INSTALLED_APPS += (
--- manage.py
+++ manage.py
@@ -2,12 +2,19 @@
 import os
 import sys
 
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "configs.settings.dev")
 
 if __name__ == "__main__":
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
-from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
+try:
+    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+except ImportError:
+    pass
+else:
+    application = Sentry(application)

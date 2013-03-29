421c95978e30c4b9cb93027f0f5712f2dbf4e9da
Saul Shanabrook <s.shanabrook@gmail.com>
2013-03-28 23:54:57 -0400

    Made local storage the default

--- configs/settings/dev.py
+++ configs/settings/dev.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
 #########
 # DEBUG #
 #########
-DEBUG = TEMPLATE_DEBUG = False
+DEBUG = TEMPLATE_DEBUG = True
 
 INSTALLED_APPS += ('debug_toolbar',)
 MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
@@ -22,3 +22,11 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 )
+
+if not os.environ.get('USE_S3_IN_DEV', None):
+    STATICFILES_STORAGE = COMPRESS_STORAGE = DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+    STATIC_URL = '/static/'
+    STATIC_ROOT = rel_path('tmp/static')
+
+    MEDIA_ROOT = rel_path('tmp/media')
+    MEDIA_URL = '/media/'
--- configs/urls.py
+++ configs/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from .sitemaps import sitemaps
 
@@ -30,6 +33,10 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
 )
 
-urlpatterns += patterns('django.contrib.flatpages.views',
+urlpatterns += patterns(
+    'django.contrib.flatpages.views',
     url(r'^contact/$', 'flatpage', {'url': '/contact/'}, name='contact')
 )
+if settings.DEFAULT_FILE_STORAGE == 'django.core.files.storage.FileSystemStorage':
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()

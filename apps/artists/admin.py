f3a7564b5f1a5b794c0f7880c15d23c59b436495
Saul Shanabrook <s.shanabrook@gmail.com>
2013-04-13 21:38:28 -0400

    more admin prettifying, this time with artists

--- apps/artists/admin.py
+++ apps/artists/admin.py
@@ -6,8 +6,8 @@ from apps.photos.admin import PhotoInline
 
 class ArtistAdmin(admin.ModelAdmin):
     inlines = [PhotoInline]
-    fields = (('first_name', 'last_name'), 'visible', 'resume',)
+    fields = (('first_name', 'last_name'), 'visible',)
     list_display = ('first_name', 'last_name', 'visible')
-
+    list_display_links = ('first_name', 'last_name')
 
 admin.site.register(Artist, ArtistAdmin)

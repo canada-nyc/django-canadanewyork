b19d947f0a1d3ca3b3ea3cc676abd9c350f18c0a
Saul Shanabrook <s.shanabrook@gmail.com>
2013-04-14 13:54:05 -0400

    fixed photos factory creation to specify content object

--- tests/apps/photos/related_factories.py
+++ tests/apps/photos/related_factories.py
@@ -4,5 +4,5 @@ def create_photos(self, create, extracted, **kwargs):
         self.photos.add(extracted)
     number = int(kwargs.pop('n', 0))
     for _ in range(number):
-        photo = PhotoFactory.create(**kwargs)
+        photo = PhotoFactory.create(content_object=self, **kwargs)
         self.photos.add(photo)

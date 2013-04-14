e90d40e23bc7c267601f8ceaa239bc2420b7d89d
Saul Shanabrook <s.shanabrook@gmail.com>
2013-04-13 22:18:02 -0400

    Add `full_title` property to press

--- apps/press/models.py
+++ apps/press/models.py
@@ -14,7 +14,11 @@ class Press(url_tracker.URLTrackingMixin, models.Model):
     def image_path(instance, filename):
         return os.path.join(instance.get_absolute_url()[1:], 'content', filename)
 
-    title = models.CharField(max_length=500)
+    title = models.CharField(
+        max_length=500,
+        blank=True,
+        help_text='If this is blank then the exhibition or artist will be used instead. Also when this is displayed on, it will be prefixed by the publisher, if one is set'
+    )
     link = models.URLField(null=True, blank=True, verbose_name=u'External link')
     content = models.TextField(blank=True)
     content_file = models.FileField(upload_to=image_path, blank=True, null=True, max_length=500)
@@ -25,8 +29,8 @@ class Press(url_tracker.URLTrackingMixin, models.Model):
     artist = models.ForeignKey(Artist, blank=True, null=True, related_name='press')
     exhibition = models.ForeignKey(Exhibition, blank=True, null=True, related_name='press',)
     slug = SlugifyField(
-        populate_from=('get_year', 'publisher', 'title',),
-        slug_template=u'{}/{}-{}',
+        populate_from=('date_year', '__unicode__',),
+        slug_template=u'{}/{}',
         unique=True
     )
 
@@ -36,8 +40,8 @@ class Press(url_tracker.URLTrackingMixin, models.Model):
 
     def __unicode__(self):
         if self.publisher:
-            return u'{}: {}'.format(self.publisher, self.title)
-        return self.title
+            return u'{}: {}'.format(self.publisher, self.full_title)
+        return self.full_title
 
     def clean(self):
 
@@ -49,11 +53,18 @@ class Press(url_tracker.URLTrackingMixin, models.Model):
         return reverse('press-detail', kwargs={'slug': self.slug})
 
     @property
-    def get_year(self):
-        '''
-        For slug
-        '''
+    def date_year(self):
         return self.date.year
 
+    @property
+    def full_title(self):
+        'for display in template, prepended by publisher'
+        if self.title:
+            return self.title
+        elif self.exhibition:
+            return unicode(self.exhibition)
+        elif self.artist:
+            return unicode(self.artist)
+        return ''
 
 url_tracker.track_url_changes_for_model(Press)

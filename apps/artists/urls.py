48b1d6730ca32a30beb39b2761560e153eae732b
Saul Shanabrook <s.shanabrook@gmail.com>
2013-04-15 17:40:08 -0400

    Added artist-exhibition-list

--- apps/artists/urls.py
+++ apps/artists/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import patterns, url
 
-from .views import ArtistList, ArtistDetail, ArtistPressDetail, ArtistResume
+from .views import ArtistList, ArtistDetail, ArtistPressList, ArtistResume, ArtistExhibitionList
+
 
 urlpatterns = patterns(
     '',
     url(r'^$', ArtistList.as_view(), name='artist-list'),
     url(r'^(?P<slug>[-\w]+)/$', ArtistDetail.as_view(), name='artist-detail'),
-    url(r'^(?P<slug>[-\w]+)/press/$',
-        ArtistPressDetail.as_view(),
-        name='artist-press-list'),
-    url(r'^(?P<slug>[-\w]+)/resume/$',
-        ArtistResume.as_view(),
-        name='artist-resume')
+    url(r'^(?P<slug>[-\w]+)/press/$', ArtistPressList.as_view(), name='artist-press-list'),
+    url(r'^(?P<slug>[-\w]+)/exhibitions/$', ArtistExhibitionList.as_view(), name='artist-exhibition-list'),
+    url(r'^(?P<slug>[-\w]+)/resume/$', ArtistResume.as_view(), name='artist-resume')
 )
--- apps/artists/views.py
+++ apps/artists/views.py
@@ -11,9 +11,26 @@ class ArtistDetail(DetailView):
     queryset = Artist.in_gallery.all()
 
 
-class ArtistPressDetail(DetailView):
+class ArtistPressList(DetailView):
     queryset = Artist.in_gallery.all()
     template_name = 'press/press_list.html'
+    context_object_name = "related_object"
+
+    def get_context_data(self, **kwargs):
+        context = super(ArtistPressList, self).get_context_data(**kwargs)
+        context['press_list'] = context['related_object'].all_press
+        return context
+
+
+class ArtistExhibitionList(DetailView):
+    queryset = Artist.in_gallery.all()
+    template_name = 'exhibitions/exhibition_list.html'
+    context_object_name = "related_object"
+
+    def get_context_data(self, **kwargs):
+        context = super(ArtistExhibitionList, self).get_context_data(**kwargs)
+        context['exhibition_list'] = context['related_object'].exhibitions.all()
+        return context
 
 
 class ArtistResume(DetailView):

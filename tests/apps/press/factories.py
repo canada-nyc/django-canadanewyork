ee25eae112731911c132aa45691c73c761800bdb
Saul Shanabrook <s.shanabrook@gmail.com>
2013-04-16 10:59:24 -0400

    press factory create content file

--- tests/apps/press/factories.py
+++ tests/apps/press/factories.py
@@ -1,8 +1,11 @@
 import factory
 
+from django.core.files.base import ContentFile
+
 from apps.press.models import Press
 from ..artists.related_factories import create_artist
 from ..exhibitions.related_factories import create_exhibition
+from ... import utils
 
 
 class PressFactory(factory.DjangoModelFactory):
@@ -14,3 +17,14 @@ class PressFactory(factory.DjangoModelFactory):
 
     exhibition = factory.PostGeneration(create_exhibition)
     artist = factory.PostGeneration(create_artist)
+
+    @factory.post_generation
+    def content_file(self, create, extracted, **kwargs):
+        if extracted:
+            file_name, file = extracted
+        elif kwargs.pop('make', None):
+            file_name = 'image.jpg'
+            file = ContentFile('Some file stuff')
+        else:
+            return
+        self.content_file.save(file_name, file)

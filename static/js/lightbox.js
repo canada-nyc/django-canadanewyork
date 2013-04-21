0f838859dd9c5ef2bdc635249e00c66b03ed47b8
Tim Evans <tim-evans@sproutcore.com>
2013-04-21 18:24:15 -0400

    #107 add scaling to the lightbox
    this might need some fiddling to ensure correctness

--- static/js/lightbox.js
+++ static/js/lightbox.js
@@ -20,6 +20,7 @@ CANADA.Lightbox = function () {
   $('.lightbox .prev') .on('click', bind(this.prev, this));
 
   $(document).on('keydown', bind(this.interpretKeyboardEvent, this));
+  $(window).on('resize', bind(this.frameSizeDidChange, this));
 };
 
 CANADA.Lightbox.prototype = {
@@ -63,6 +64,7 @@ CANADA.Lightbox.prototype = {
     if (this.swipe) this.swipe.kill();
     $(this.element).remove();
     this.element = null;
+    this.model = null;
   },
 
   show: function (galleryId) {
@@ -70,6 +72,7 @@ CANADA.Lightbox.prototype = {
 
     $('.lightbox').append(CANADA.render('gallery', gallery));
 
+    this.model   = gallery;
     this.element = $('#gallery-' + galleryId)[0];
     if (gallery.photos.length == 1) {
       $('.lightbox').find('.next, .prev').hide();
@@ -92,6 +95,9 @@ CANADA.Lightbox.prototype = {
       $figure = $img.parent();
       $figure.prepend('<div class="throbber" style="width: ' + image.width + 'px; height: ' + image.height + 'px"></div>');
       $img.imagesLoaded(bind(this.imageLoaded, this, photo));
+
+      // Scale the photos when we first show the lightbox
+      this.scalePhotoToFit(photo);
     }
   },
 
@@ -106,6 +112,50 @@ CANADA.Lightbox.prototype = {
 
     $figure.find('.throbber').hide();
     $img.fadeIn();
+  },
+
+  frameSizeDidChange: function () {
+    // Only retile if we're showing content
+    if (this.element) {
+
+      // Scale all of the photos in this gallery to fit
+      // the screen properly
+      var photos = this.model.photos;
+      for (var i = 0, len = photos.length; i < len; i++) {
+        this.scalePhotoToFit(photos[i]);
+      }
+    }
+  },
+
+  scalePhotoToFit: function (photo) {
+    var image    = photo.sizes.large || photo.sizes.thumb,
+        $img     = $('#photo-' + photo.id),
+        $figure  = $img.parent(),
+        $caption = $figure.find('aside');
+
+    // Resize the photo so the caption and photo are both
+    // completely visible.
+    var availableHeight = $figure.innerHeight() -
+                          $img.offset().top -
+                          $caption.outerHeight(),
+        scale = 1;
+
+    // Scale down the image
+    if (image.height > availableHeight) {
+      scale = availableHeight / image.height;
+    }
+
+    // Scale and center the image
+    if (scale < 1) {
+      $img.css({
+        height:     (scale * image.height)    + "px",
+        marginLeft: (scale * image.width / 2) + "px"
+      });
+
+    // Remove styling to let the browser do it's thing
+    } else {
+      $img.removeAttr('style');
+    }
   }
 
 };

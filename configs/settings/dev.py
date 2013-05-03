5ddec0625b2fd27942137e2830ad72f1674de346
Saul Shanabrook <s.shanabrook@gmail.com>
2013-05-02 21:59:45 -0400

    removed debug in dev settings

--- configs/settings/dev.py
+++ configs/settings/dev.py
@@ -4,21 +4,22 @@ from .local_storage import *
 #########
 # LOCAL #
 #########
-INTERNAL_IPS = ('127.0.0.1',)
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['localhost', 'localhost:8000' '127.0.0.1', '127.0.0.1:8000']
 
 #########
 # DEBUG #
 #########
-DEBUG = TEMPLATE_DEBUG = True
 
-INSTALLED_APPS += ('debug_toolbar',)
-MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
-DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-}
 
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
+# DEBUG = TEMPLATE_DEBUG = True
+
+# INSTALLED_APPS += ('debug_toolbar',)
+# MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+# DEBUG_TOOLBAR_CONFIG = {
+#     'INTERCEPT_REDIRECTS': False,
+# }
+
+# TEMPLATE_LOADERS = (
+#     'django.template.loaders.filesystem.Loader',
+#     'django.template.loaders.app_directories.Loader',
+# )

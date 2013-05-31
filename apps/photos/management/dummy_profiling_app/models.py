7c5018c95295696bafb10e4b670d2ff3cd8f3359
Saul Shanabrook <s.shanabrook@gmail.com>
2013-05-31 00:47:27 -0400

    Revert "add testing for photo timing"
    This reverts commit 919bf42e591d13dc7178922e27e2416b989462c4.
    
    Conflicts:
    	configs/settings/common.py

--- apps/photos/management/dummy_profiling_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.contenttypes import generic
-
-from apps.photos.models import Photo
-
-
-class RelatedPhotoModel(models.Model):
-
-    photos = generic.GenericRelation(Photo)

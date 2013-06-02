47934cd1ad7a8e414a6d7a477f9e0f9f2e491f01
Saul Shanabrook <s.shanabrook@gmail.com>
2013-06-02 09:54:26 -0400

    fixed logging formatting for import_wp

--- libs/import_wp/management/commands/import_wp.py
+++ libs/import_wp/management/commands/import_wp.py
@@ -86,6 +86,7 @@ class Command(BaseCommand):
                                 L += 1
                                 L('Duplicate, not added')
                                 L -= 1
+                        L -= 1
 
         L('Cleaning models')
 

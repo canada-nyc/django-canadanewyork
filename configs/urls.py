6cbf67ceedd348461edcc124dc4809e18ab23ace
Saul Shanabrook <s.shanabrook@gmail.com>
2013-07-09 19:33:40 -0400

    properly serve static when debug is false and serving locally

--- configs/urls.py
+++ configs/urls.py
@@ -52,4 +52,7 @@ if loader_io_verification:
     )
 
 if settings.DEFAULT_FILE_STORAGE == 'django.core.files.storage.FileSystemStorage':
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )

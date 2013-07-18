3bbee7927262281e529d98d8bf260d00c28d6101
Saul Shanabrook <s.shanabrook@gmail.com>
2013-07-18 11:52:57 -0400

    Removed global press list, fixed #135

--- apps/press/urls.py
+++ apps/press/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import PressDetail, PressList
+from .views import PressDetail
 
 
 urlpatterns = patterns(
     '',
-    url(r'^$', PressList.as_view(), name='press-list'),
     url(r'^(?P<slug>\d{4}/[-\w]*)', PressDetail.as_view(), name='press-detail'),
 )
--- apps/press/views.py
+++ apps/press/views.py
@@ -1,12 +1,7 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView
 
 from .models import Press
 
 
 class PressDetail(DetailView):
     model = Press
-
-
-class PressList(ListView):
-    model = Press
-    context_object_name = 'press_list'

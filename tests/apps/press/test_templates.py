28c8336969b0d3a522b814dcffb734a86bad0e1f
Saul Shanabrook <s.shanabrook@gmail.com>
2013-07-20 11:19:42 -0400

    removed press list tests, because press list is now gone

--- tests/apps/press/test_templates.py
+++ tests/apps/press/test_templates.py
@@ -9,32 +9,6 @@ from ..artists.factories import ArtistFactory
 from ..exhibitions.factories import ExhibitionFactory
 
 
-class PressListTest(WebTest):
-    def test_reverse(self):
-        self.app.get(
-            reverse('press-list')
-        )
-
-    def test_nav_click(self):
-        press_list = self.app.get(
-            reverse('press-list')
-        )
-        press_list.click(
-            'Press',
-            href=reverse('press-list')
-        )
-
-    def test_click_press(self):
-        Press = PressFactory()
-        press_list = self.app.get(
-            reverse('press-list')
-        )
-        press_list.click(
-            unicode(Press),
-            href=reverse('press-detail', kwargs={'slug': Press.slug, })
-        )
-
-
 class PressDetailTest(WebTest):
     def test_unicode(self):
         Press = PressFactory()
@@ -45,11 +19,6 @@ class PressDetailTest(WebTest):
         Press = PressFactory(content='content stuff')
         press_detail = self.app.get(Press.get_absolute_url())
         self.assertIn(Press.content, press_detail)
-
-    def test_external_link(self):
-        Press = PressFactory(link='http://some_site.com')
-        press_detail = self.app.get(Press.get_absolute_url())
-        press_detail.click(href=Press.link)
 
     def test_content_file_link(self):
         Press = PressFactory()

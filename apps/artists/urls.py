bd635f31f57ed28745bb2072beab7ad5dd8d7ef2
Saul Shanabrook <s.shanabrook@gmail.com>
2013-07-22 11:28:10 -0400

    Moved press views to import whole module and then use dot notation to selection view

--- apps/artists/urls.py
+++ apps/artists/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import patterns, url
 
-from .views import ArtistList, ArtistDetail, ArtistPressList, ArtistResume, ArtistExhibitionList
-
+from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^$', ArtistList.as_view(), name='artist-list'),
-    url(r'^(?P<slug>[-\w]+)/$', ArtistDetail.as_view(), name='artist-detail'),
-    url(r'^(?P<slug>[-\w]+)/press/$', ArtistPressList.as_view(), name='artist-press-list'),
-    url(r'^(?P<slug>[-\w]+)/exhibitions/$', ArtistExhibitionList.as_view(), name='artist-exhibition-list'),
-    url(r'^(?P<slug>[-\w]+)/resume/$', ArtistResume.as_view(), name='artist-resume')
+    url(r'^$', views.ArtistList.as_view(), name='artist-list'),
+    url(r'^(?P<slug>[-\w]+)/$', views.ArtistDetail.as_view(), name='artist-detail'),
+    url(r'^(?P<slug>[-\w]+)/press/$', views.ArtistPressList.as_view(), name='artist-press-list'),
+    url(r'^(?P<slug>[-\w]+)/exhibitions/$', views.ArtistExhibitionList.as_view(), name='artist-exhibition-list'),
+    url(r'^(?P<slug>[-\w]+)/resume/$', views.ArtistResume.as_view(), name='artist-resume')
 )

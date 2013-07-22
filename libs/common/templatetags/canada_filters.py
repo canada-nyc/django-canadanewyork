518ce37f12eaf5dbad26dd6d36716ecf46011eb4
Saul Shanabrook <s.shanabrook@gmail.com>
2013-07-22 08:59:44 -0400

    Add pudb debug template filer

--- libs/common/templatetags/canada_filters.py
+++ libs/common/templatetags/canada_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from pudb import set_trace
 
 
 register = template.Library()
@@ -9,3 +10,9 @@ def getattribute(value, arg):
     """Gets an attribute of an object dynamically from a string name"""
     if hasattr(value, arg):
         return getattr(value, arg)
+
+
+@register.filter
+def pudb(element):
+    set_trace()
+    return element

af5c4b40825b01df8d7025b2547b1dcb0ece1b0d
Saul Shanabrook <s.shanabrook@gmail.com>
2013-08-08 11:03:38 -0400

    Corrected test for book url to reflect new description (#157)

--- tests/apps/books/test_models.py
+++ tests/apps/books/test_models.py
@@ -10,6 +10,7 @@ class BookGetPurchaseUrl(TestCase):
             'mailto:gallery%40canadanewyork.com'
             '?subject=Purchase%20Book'
             '&body=Hello%0AI%20am%20interested%20in%20buying%20{}%20{}%3A{}.'
+            '%20Can%20you%20please%20contact%20me%20for%20pricing%20and%20availabilty%3F'
         )
         url_with_data = url.format(
             book.artist.first_name,

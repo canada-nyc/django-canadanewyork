d16ee7145f393e1caaae7c0da9aa85eb0aa37d2a
Saul Shanabrook <s.shanabrook@gmail.com>
2013-09-02 14:53:29 -0400

    Fixed getting unicorn worker number from env

--- configs/gunicorn.py
+++ configs/gunicorn.py
@@ -1,7 +1,7 @@
 import os
 import multiprocessing
 try:
-    workers = os.environ['GUNICORN_WORKERS']
+    workers = int(os.environ['GUNICORN_WORKERS'])
 except KeyError:
     workers = multiprocessing.cpu_count() * 2 + 1
 timeout = 30  # in seconds

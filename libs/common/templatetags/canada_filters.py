98adcea9432f8681249ce8231f57150ea67f7c7b
Saul Shanabrook <s.shanabrook@gmail.com>
2013-09-22 12:58:39 -0400

    remove unused custom template filters

--- libs/common/templatetags/canada_filters.py
+++ libs/common/templatetags/canada_filters.py
@@ -1,18 +1,4 @@
 from django import template
-from pudb import set_trace
 
 
 register = template.Library()
-
-
-@register.filter
-def getattribute(value, arg):
-    """Gets an attribute of an object dynamically from a string name"""
-    if hasattr(value, arg):
-        return getattr(value, arg)
-
-
-@register.filter
-def pudb(element):
-    set_trace()
-    return element

17903adbf7fd01734e423b18a4e9f6a162f5c7d4
Saul Shanabrook <s.shanabrook@gmail.com>
2013-09-22 13:08:13 -0400

    escape json without escaping all ascii characters

--- libs/common/templatetags/canada_filters.py
+++ libs/common/templatetags/canada_filters.py
@@ -1,4 +1,14 @@
-from django import template
+import json
 
+from django import template
+from django.utils.safestring import mark_safe
+from django.utils.encoding import force_text
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
+
+
+@register.filter
+@stringfilter
+def escapejson(value):
+    return mark_safe(json.dumps(force_text(value), ensure_ascii=False))

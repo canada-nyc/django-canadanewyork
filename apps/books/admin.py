045051f41c15fd56239969ce9d2a823d4f32b390
Saul Shanabrook <s.shanabrook@gmail.com>
2013-10-02 20:07:46 -0400

    Add book admin page (fixed #217)

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -5,3 +5,22 @@ from .models import Book
 
 class BookInline(admin.TabularInline):
     model = Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'date_text')
+    list_filter = ('artist',)
+    date_hierarchy = 'date'
+
+    fields = (
+        'title',
+        ('date', 'date_text'),
+        'artist'
+    )
+
+    raw_id_fields = ('artist',)
+    autocomplete_lookup_fields = {
+        'fk': ['artist'],
+        'm2m': ['related_m2m'],
+    }
+
+admin.site.register(Book, BookAdmin)

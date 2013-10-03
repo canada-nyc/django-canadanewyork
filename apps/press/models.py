7930e444043b93975be87a2d07cd7e0dcbe240fa
Saul Shanabrook <s.shanabrook@gmail.com>
2013-10-02 23:05:19 -0400

    don't allow blank fields in press (fixed #219)

--- apps/press/models.py
+++ apps/press/models.py
@@ -2,6 +2,7 @@ import os
 
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 
 import dumper
 import url_tracker
@@ -10,6 +11,8 @@ from ..artists.models import Artist
 from ..exhibitions.models import Exhibition
 from libs.slugify.fields import SlugifyField
 
+
+SLUG_FIELD_NAMES = ("publisher", "title", "artist", "exhibition")
 
 class Press(url_tracker.URLTrackingMixin, models.Model):
     def file_path(instance, filename):
@@ -51,11 +54,21 @@ class Press(url_tracker.URLTrackingMixin, models.Model):
     class Meta:
         ordering = ['-date']
         verbose_name_plural = "press"
-        unique_together = ("publisher", "title", "artist", "exhibition")
+        unique_together = SLUG_FIELD_NAMES
 
 
     def __unicode__(self):
         return self.title
+
+    @property
+    def _slug_field_values(self):
+        values = [getattr(self, field_name) for field_name in SLUG_FIELD_NAMES]
+        return filter(None, values)
+
+
+    def clean(self):
+        if not any(self._slug_field_values):
+            raise ValidationError('At least one of the following must be filled in: ' + str(SLUG_FIELD_NAMES))
 
     def get_absolute_url(self):
         return reverse('press-detail', kwargs={'slug': self.slug})
@@ -68,13 +81,7 @@ class Press(url_tracker.URLTrackingMixin, models.Model):
 
     @property
     def slug_title(self):
-        slug_fields = [
-            self.publisher,
-            self.title,
-            self.artist,
-            self.exhibition
-        ]
-        return '-'.join(map(str, filter(None, slug_fields)))
+        return '-'.join(map(str, self._slug_field_values))
 
     @property
     def date_year(self):

c139926fe454f900346e851f2ec0ff455fcc551a
Saul <s.shanabrook@gmail.com>
2013-10-27 10:58:40 -0400

    Verify that Press date_text appears in detail

--- tests/apps/press/test_templates.py
+++ tests/apps/press/test_templates.py
@@ -30,9 +30,11 @@ class PressDetailTest(WebTest):
     def test_date_text_overrides_date(self):
         year, month, day = (3000, 1, 1)
         date = datetime.datetime(year, month, day)
-        Press = PressFactory(content='_', date_text='some text', date=date)
+        date_text = 'Something or other'
+        Press = PressFactory(content='_', date_text=date_text, date=date)
         press_detail = self.app.get(Press.get_absolute_url())
         self.assertNotIn(str(year), press_detail)
+        self.assertIn(date_text, press_detail)
 
     def test_pages_range_displayed(self):
         Press = PressFactory(content='_', pages_range='some text')

a9d8398421dd0dd15aad047f46d669fb375bac13
Saul <s.shanabrook@gmail.com>
2014-01-06 16:04:27 -0500

    Removed a lot of logic from clean_db and renamed remaining to init_db
    This reverts commit 0df11385065b132f669ec431cacd11ac58cc94cb.

---
+++ libs/common/management/commands/init_db.py
@@ -0,0 +1,47 @@
+import os
+from optparse import make_option
+
+from django.core.management.base import NoArgsCommand
+from django.core.management import call_command
+from django.conf import settings
+
+
+class Command(NoArgsCommand):
+    help = 'Add initial data to blank datavase'
+    option_list = NoArgsCommand.option_list + (
+        make_option(
+            '--init',
+            action='store_true',
+            dest='init',
+            default=False,
+            help='Tells Django to add factory models'
+        ),
+    )
+
+    def handle(self, *args, **options):
+        self.log('Adding cache table')
+        call_command('createcachetable', 'cache', interactive=False, verbosity=0)
+        self.log('Initial sync')
+        call_command('syncdb', interactive=False, verbosity=0)
+        self.log('Initial migrate')
+        call_command('migrate', interactive=False, verbosity=0)
+        self.log('Adding super user')
+        call_command(
+            'create_user_permissions',
+            os.environ['ADMIN_USERNAME'],
+            os.environ['ADMIN_PASSWORD'],
+            'admin',
+            'contenttypes',
+            'url_tracker',
+            'sessions',
+            'auth'
+        )
+        call_command('set_site', settings.ALLOWED_HOSTS[0])
+        self.log('Loading contact fixture')
+        call_command('loaddata', 'configs/fixtures/contact.json')
+        if options.get('init'):
+            self.log('Adding test_data')
+            call_command('test_data')
+
+    def log(self, string):
+        self.stdout.write(string + '\n')

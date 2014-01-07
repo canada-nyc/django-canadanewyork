ac24e70e35361b9aa11783490c806197e8199db8
Saul <s.shanabrook@gmail.com>
2014-01-07 18:57:34 -0500

    Rename targets to 'dev' and 'prod'

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -15,7 +15,7 @@ ns.configure({
     'default_app_label': 'local',
     'apps': {
         'local': {'type': 'local', 'pipeline': 'canada'},
-        'heroku-dev': {'type': 'heroku', 'name': 'canada-dev', 'pipeline': 'canada'},
-        'heroku-prod': {'type': 'heroku', 'name': 'canada'}
+        'dev': {'type': 'heroku', 'name': 'canada-dev', 'pipeline': 'canada'},
+        'prod': {'type': 'heroku', 'name': 'canada'}
     }
 })

86b37b28a0e4becf2049edea06bfab2304477128
Saul <s.shanabrook@gmail.com>
2014-01-07 20:22:26 -0500

    Added newline for confirm prompt

--- tasks/base.py
+++ tasks/base.py
@@ -16,7 +16,7 @@ def _get_single_app(ctx, app_label):
     return ctx['apps'][app_label]
 
 
-def confirm(ctx, prompt='Continue?', failure_prompt='User cancelled task'):
+def confirm(ctx, prompt='Continue?\n', failure_prompt='User cancelled task'):
     '''
     Prompt the user to continue. Repeat on unknown response. Raise
     Failure Exception on negative response
@@ -49,7 +49,7 @@ def get_app(ctx, app_label):
     resolved_label = _resolve_app_label(ctx, app_label)
     print '-> {}'.format(resolved_label)
     app = _get_single_app(ctx, resolved_label)
-    confirm_app(ctx, app, prompt='Really run on {}?'.format(app_label))
+    confirm_app(ctx, app, prompt='Really run on {}?\n'.format(app_label))
     return app
 
 
@@ -62,7 +62,7 @@ def get_apps(ctx, source_app_label, destination_app_label):
     print '{} -> {}'.format(source_resolved_label, destination_resolved_label)
     source_app, destination_app = [_get_single_app(ctx, source_resolved_label), _get_single_app(ctx, destination_resolved_label)]
 
-    confirm_app(ctx, destination_app, prompt='Really run on {}?'.format(destination_app_label))
+    confirm_app(ctx, destination_app, prompt='Really run on {}?\n'.format(destination_app_label))
 
     if source_app == destination_app:
         raise ParseError('Source and destination apps are the same')

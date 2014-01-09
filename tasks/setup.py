445b6654cda3bd662473daabedd7527d7ba7a501
Saul <s.shanabrook@gmail.com>
2014-01-08 20:44:42 -0500

    Run setup tasks with context

--- tasks/setup.py
+++ tasks/setup.py
@@ -1,8 +1,8 @@
-from invoke import task, run
+from invoke import ctask as task
 
 
 @task
-def dev():
-    run('pip install -r requirements.txt')
-    run('mkdir -p tmp')
-    run('npm install --global --production "less" "git://github.com/mishoo/UglifyJS2.git#3bd7ca9961125b39dcd54d2182cb72bd1ca6006e"')
+def dev(ctx):
+    ctx.run('pip install -r requirements.txt')
+    ctx.run('mkdir -p tmp')
+    ctx.run('npm install --global --production "less" "git://github.com/mishoo/UglifyJS2.git#3bd7ca9961125b39dcd54d2182cb72bd1ca6006e"')

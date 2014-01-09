c52cca73d36e0ead62ee7f8d937aa4d1019dd4bc
Saul <s.shanabrook@gmail.com>
2014-01-08 19:06:27 -0500

    Provide better logging for management commands when trying to use S3 and not set up for it

--- libs/common/management/commands/delete_canada_static.py
+++ libs/common/management/commands/delete_canada_static.py
@@ -1,4 +1,4 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import NoArgsCommand, CommandError
 from django.conf import settings
 from django.core.files.storage import default_storage
 
@@ -8,8 +8,11 @@ class Command(NoArgsCommand):
 
     def handle(self, *args, **options):
         prefix = 'canada/'
-        self.log(
-            '    Found bucket {}'.format(settings.AWS_STORAGE_BUCKET_NAME))
+        try:
+            self.log(
+                '    Found bucket {}'.format(settings.AWS_STORAGE_BUCKET_NAME))
+        except AttributeError:
+            raise CommandError('Must be using S3 to delete only static')
         self.log('    Getting connection...')
         connection = default_storage.connection
         self.log('    Getting bucket...')

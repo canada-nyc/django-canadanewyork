e1a618cf9bf652b382cf142c1c2e941600215812
Saul <s.shanabrook@gmail.com>
2014-02-09 14:14:35 -0500

    Don't require model validation for getting database name

--- libs/common/management/commands/database_name.py
+++ libs/common/management/commands/database_name.py
@@ -5,6 +5,7 @@ from django.core.management.base import NoArgsCommand
 
 class Command(NoArgsCommand):
     help = 'prints the database name'
+    requires_model_validation = False
 
     def handle(self, *args, **options):
         self.stdout.write(settings.DATABASES['default']['NAME'], ending='')

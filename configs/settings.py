2172df35bd2debb48afa67beb43d2f20c270f03d
Saul <s.shanabrook@gmail.com>
2014-02-12 16:37:38 -0500

    Let django-debug-toolbar set itself up with new version

--- configs/settings.py
+++ configs/settings.py
@@ -297,24 +297,9 @@ if get_env_variable('CANADA_DEBUG_TOOLBAR'):
     INSTALLED_APPS += (
         'debug_toolbar',
     )
-    MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     DEBUG_TOOLBAR_CONFIG = {
-        'INTERCEPT_REDIRECTS': False,
         'SHOW_TOOLBAR_CALLBACK': lambda _: True,
     }
-    DEBUG_TOOLBAR_PANELS = (
-        'debug_toolbar.panels.cache.CacheDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-        'debug_toolbar.panels.profiling.ProfilingDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.version.VersionDebugPanel',
-    )
 
 
 ###########

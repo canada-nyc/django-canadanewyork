8d1268643cb310a8b13c8a9c3f45872ec824808f
Saul <s.shanabrook@gmail.com>
2014-02-25 19:27:56 -0500

    Make sure CKEditor as_html won't be escaped in templates

--- tests/libs/ckeditor/test_fields.py
+++ tests/libs/ckeditor/test_fields.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.test.utils import override_settings
+from django.template import Context, Template
 
 from .models import CKEditorModel
 from ...utils import AddAppMixin
@@ -10,7 +11,15 @@ class CKEditorTest(AddAppMixin, TestCase):
     custom_apps = ('tests.libs.ckeditor',)
 
     def setUp(self):
-        self.model = CKEditorModel.objects.create(html='test')
+        self.model = CKEditorModel.objects.create(html='<p>test</p>')
+        self.target_html = '<div class="hey"><p>test</p></div>\n'
 
     def test_as_html_attribute(self):
-        self.assertEqual(self.model.html.as_html, '<div class="hey">test</div>\n')
+        self.assertEqual(self.model.html.as_html, self.target_html)
+
+    def test_wont_escape_in_template(self):
+        rendered = Template('{{ model.html.as_html }}').render(
+            Context({'model': self.model})
+        )
+
+        self.assertEqual(rendered, self.target_html)

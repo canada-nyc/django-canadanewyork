3cca6d48764d795da74a9f8f761d18f2fc2f935b
Saul <s.shanabrook@gmail.com>
2014-02-25 19:00:55 -0500

    Remove old CKEditor Admin Widget

--- apps/updates/admin.py
+++ apps/updates/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 from apps.photos.admin import photo_inline
-from libs.common.admin import editor_form
 from .models import Update, UpdatePhoto
 
 
@@ -14,7 +13,6 @@ class UpdatePhotoInline(photo_inline(UpdatePhoto)):
 
 
 class UpdateAdmin(admin.ModelAdmin):
-    form = editor_form(['description'])
     inlines = [UpdatePhotoInline]
     date_hierarchy = 'post_date'
     list_display = ('post_date',)
